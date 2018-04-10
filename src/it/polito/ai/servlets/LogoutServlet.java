35fd24943f5ec8c8241b7c9004b357ced77e23cd
simone <simone.riggi92@gmail.com>
2018-04-10 08:30:07 +0200

    Login+Post_Positions+Get_Positions+Logout

---
+++ src/it/polito/ai/servlets/LogoutServlet.java
@@ -0,0 +1,25 @@
+package it.polito.ai.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutServlet  extends HttpServlet {
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+       response.setContentType("text/html");
+
+       PrintWriter pw = response.getWriter();
+       HttpSession session = request.getSession();
+       session.invalidate();
+
+       pw.println("Logout effettuato!");
+       pw.close();
+   }
+}

6c34875f22ae6be5233717c9d281057d67f00093
Alberto <Alberto>
2018-04-12 10:33:55 +0200

    risposta json ad una get del client fatta! sarebbe bello avere degli output del server e del client più facili da leggere

--- src/it/polito/ai/servlets/RangeServlet.java
+++ src/it/polito/ai/servlets/RangeServlet.java
@@ -1,6 +1,6 @@
 package it.polito.ai.servlets;
 
-//import com.sun.org.apache.xpath.internal.FoundIndex;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import it.polito.ai.utilities.Position;
 import it.polito.ai.utilities.User;
 
@@ -17,7 +17,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- *      Servlet che restituisce un sottoinsieme di posizioni in un dato range temporale
+ *  Servlet che restituisce un sottoinsieme di posizioni in un dato range temporale
  */
 
 @WebServlet(urlPatterns = "/rangePositions")   //???? url
@@ -25,93 +25,73 @@ public class RangeServlet extends HttpServlet{
 
     ConcurrentHashMap<String, User> users;
     User currentUser;
+    String currentUsername;
+    LinkedList<Position> rangeList = new LinkedList<>();
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
+        // recupera la mappa degli users
         users = (ConcurrentHashMap<String, User>) this.getServletConfig()
                 .getServletContext()
                 .getAttribute("users");
 
+        // recupera la sessione corrente, se esiste (dovrebbe sempre esistere perchè se no agirebbe il filtro)
         HttpSession session = req.getSession(false);
+        String currentUsername = (String)session.getAttribute("user");
+
+        // verifica dei parametri passato dal client nella richiesta
         if(req.getParameter("startTimestamp") == null || req.getParameter("endTimestmap")== null) {
+            // la richiesta del client non è valida
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             resp.reset();
         }
 
+        // recupera i parametri della richiesta del client
         long startTimestamp = Long.parseLong(req.getParameter("startTimestamp"));
         long endTimestamp = Long.parseLong(req.getParameter("endTimestamp"));
 
-        System.out.println("Start: " + startTimestamp);
-        System.out.println("End: " + endTimestamp);
 
-        LinkedList<Position> rangeList = new LinkedList<>();
+        if (currentUsername != null
+                && !users.get(currentUsername).getPositionList().isEmpty()) {
+            currentUser = users.get(currentUsername);
+            // la lista delle posizioni dell'utente non è vuota
 
-        if (session.getAttribute("user") != null
-                && !users.get(session.getAttribute("user")).getPositionList().isEmpty()) {
-
-            currentUser = users.get(session.getAttribute("user"));
-
-            System.out.println("SIZE(GET): " + currentUser.getPositionList().size());
-            //System.out.println("ULTIMO: "+currentUser.getPositionList().getLast().getTimeStamp());
-            //se endTimeStamp > ultimo TimeStamp della lista-> endTimeStamp aggiornato
+            // se endTimestamp > ultimo timestamp della lista, allora aggiorna endTimeStamp all'ultimo timestamp della lista
             if (endTimestamp > currentUser.getPositionList().getLast().getTimeStamp()) {
                 endTimestamp = currentUser.getPositionList().getLast().getTimeStamp();
             }
 
+            // se starTimestamp < primo timestamp della lista, allora aggiorna startTimestamp al primo timestamp della lista
             if (startTimestamp < currentUser.getPositionList().getFirst().getTimeStamp()) {
                 startTimestamp = currentUser.getPositionList().getFirst().getTimeStamp();
             }
 
+            // non è ammesso il caso starTimeStamp > endTimestamp
             if (startTimestamp > endTimestamp) {
                 resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 resp.reset();
             }
 
-//            for (Position pos : currentUser.getPositionList()) {
-//                System.out.println("val: " + pos.getLatitude() + "-" + pos.getLongitude() + "-" + pos.getTimeStamp());
-//                if (pos.getTimeStamp() == startTimestamp) {
-//                    found = true;
-//                    break;
-//                }
-//            }
-//
-//            if (found) {
-//                found = false;
-//                for (Position pos : currentUser.getPositionList()) {
-//                    if (pos.getTimeStamp() == endTimestamp) {
-//                        found = true;
-//                        break;
-//                    }
-//                }
-//            }
-
-//            if (found) {
-                for (Position p : currentUser.getPositionList()) { //currentList){
-                    if (p.getTimeStamp() >= startTimestamp && p.getTimeStamp() <= endTimestamp) {
-                        System.out.println("Lat: " + p.getLatitude() + " Lon: " + p.getLongitude() + " TS: " + p.getTimeStamp());
-                        rangeList.add(p);
-                    }
+            // riempi la lista rangeList con le posizioni che rientrano nel range richiesto dal client
+            for (Position p : currentUser.getPositionList()) {
+                if (p.getTimeStamp() >= startTimestamp && p.getTimeStamp() <= endTimestamp) {
+                    rangeList.add(p);
                 }
-
-                PrintWriter pw = resp.getWriter();
-
-                int i = 1;
-                System.out.println("#Positions: " + rangeList.size());
-                for (Position p : rangeList) {
-                    pw.println("Position " + i + " --- Latitude: " + p.getLatitude()
-                            +  " --- Longitude: " + p.getLongitude() + " --- TimeStamp: " + convertTime(p.getTimeStamp()));
-                    i++;
-                }
-            //}
-
+            }
+            // crea e manda oggetto json di risposta
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getWriter(), rangeList);      // scrivi rangeList nella response come oggetto json
+            resp.setContentType("application/json");             // la risposta e' di tipo application/json
+            resp.setStatus(HttpServletResponse.SC_OK);
         }
-    }
-
-    public String convertTime(long time){
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss z");
-        Date date = new Date(time*1000L);
-        return sdf.format(date);
+        else {
+            // la lista dell'utente corrente è vuota!
+            // crea e manda oggetto json di risposta
+            ObjectMapper mapper = new ObjectMapper();
+            HashMap<String, String> emptyResponse = new HashMap<>();
+            emptyResponse.put("response", "empty");
+            mapper.writeValue(resp.getWriter(), emptyResponse);  // scrivi emptyResponse nella response come oggetto json
+            resp.setContentType("application/json");             // la risposta e' di tipo application/json
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 }
